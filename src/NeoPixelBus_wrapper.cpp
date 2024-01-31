beadc03a3f7992bd68060d9d5fd18121e7d94b89
Ton Huisman <tonhu2004@hotmail.com>
2024-01-31 22:33:34 +0100

    [Fix] Add optional `NEOPIXEL_WRAPPER_USE_ADAFRUIT` compatibility for ESP8266

--- src/NeoPixelBus_wrapper.cpp
+++ src/NeoPixelBus_wrapper.cpp
@@ -5,31 +5,42 @@
 NeoPixelBus_wrapper::NeoPixelBus_wrapper(uint16_t     _maxPixels,
                                          int16_t      _gpioPin,
                                          neoPixelType _stripType)
-  : numLEDs(_maxPixels) {
+  #ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+  : numLEDs(_maxPixels)
+  #else // ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+  : Adafruit_NeoPixel(_maxPixels, _gpioPin, _stripType)
+  #endif // ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+{
+  #ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+
   if (NEO_GRBW == (_stripType & NEO_GRBW)) {
-    #ifdef ESP8266
+    # ifdef ESP8266
     neopixels_grbw = new (std::nothrow) NEOPIXEL_LIB<NeoGrbwFeature, METHOD>(_maxPixels);
-    #endif // ifdef ESP8266
-    #ifdef ESP32
+    # endif // ifdef ESP8266
+    # ifdef ESP32
     neopixels_grbw = new (std::nothrow) NEOPIXEL_LIB<NeoGrbwFeature, METHOD>(_maxPixels, _gpioPin);
-    #endif // ifdef ESP32
+    # endif // ifdef ESP32
   } else if (NEO_GRB == (_stripType & NEO_GRB)) {
-    #ifdef ESP8266
+    # ifdef ESP8266
     neopixels_grb = new (std::nothrow) NEOPIXEL_LIB<NeoGrbFeature, METHOD>(_maxPixels);
-    #endif // ifdef ESP8266
-    #ifdef ESP32
+    # endif // ifdef ESP8266
+    # ifdef ESP32
     neopixels_grb = new (std::nothrow) NEOPIXEL_LIB<NeoGrbFeature, METHOD>(_maxPixels, _gpioPin);
-    #endif // ifdef ESP32
+    # endif // ifdef ESP32
   }
+  #endif // ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
 }
 
 NeoPixelBus_wrapper::~NeoPixelBus_wrapper() {
+  #ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
   delete neopixels_grb;
   neopixels_grb = nullptr;
   delete neopixels_grbw;
   neopixels_grbw = nullptr;
+  #endif // ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
 }
 
+#ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
 void NeoPixelBus_wrapper::begin() {
   if (nullptr != neopixels_grb) {
     neopixels_grb->Begin();
@@ -105,4 +116,5 @@ uint32_t NeoPixelBus_wrapper::getPixelColor(uint16_t n) {
   return 0u; // Fall-through value...
 }
 
+#endif // ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
 #endif // ifndef _NEOPIXELBUS_WRAPPER_CPP
